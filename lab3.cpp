2848acde28bbd489fa97c23f074e16224bced086
John Toniolo <john_toniolo9@hotmail.com>
2016-09-22 17:18:13 -0400

    added lab3
    lab3 added

---
+++ lab3.cpp
@@ -0,0 +1,239 @@
+bash-3.2$ pwd
+/
+bash-3.2$ ls
+Applications			backup				net
+Incompatible Software		bin				opt
+Library				cores				private
+Network				dev				sbin
+System				efi				sockets.log
+User Information		etc				tmp
+Users				home				usr
+Volumes				installer.failurerequests	var
+bash-3.2$ cd users/johntoniolo
+bash-3.2$ cd documents/code/cpp
+bash-3.2$ ls
+README.txt	lab2		lab22.cpp	lab3.cpp	manip.cpp~	tester
+lab1.cpp	lab2.cpp	lab23		manip		temp		tester.cpp
+lab1.cpp~	lab22		lab23.cpp	manip.cpp	temp.cpp	tester.cpp~
+bash-3.2$ clear
+bash-3.2$ ls
+README.txt	lab2		lab22.cpp	lab3.cpp	manip.cpp~	tester
+lab1.cpp	lab2.cpp	lab23		manip		temp		tester.cpp
+lab1.cpp~	lab22		lab23.cpp	manip.cpp	temp.cpp	tester.cpp~
+bash-3.2$ ./lab3
+Welcome to my Application!
+--------------------------
+
+enter elapsed time in seconds: 
+65
+your time is
+1:0:5
+type any key to exit
+bash-3.2$ ./lab3
+Welcome to my Application!
+--------------------------
+
+enter elapsed time in seconds: 
+67
+your time is
+1:0:7
+task complete, program terminatingbash-3.2$ 
+bash-3.2$ 
+bash-3.2$ 
+bash-3.2$ ./lab3
+Welcome to my Application!
+--------------------------
+
+enter elapsed time in seconds: 
+154.2
+your time is
+2:0:34
+task complete, program terminating
+bash-3.2$ ./lab3
+Welcome to my Application!
+--------------------------
+
+enter elapsed time in seconds: 
+09876
+your time is
+164:0:36
+Task complete... Program terminating
+bash-3.2$ ./lab3
+Welcome to my Application!
+--------------------------
+
+enter elapsed time in seconds: 
+68.5
+
+your time is
+1:0:8
+Task complete... Program terminating
+bash-3.2$ ./lab3
+Welcome to my Application!
+--------------------------
+
+enter elapsed time in seconds: 
+660
+
+your time is
+11:0:0
+
+Task complete... Program terminating
+bash-3.2$ ./lab3
+Welcome to my Application!
+--------------------------
+
+enter elapsed time in whole seconds, do not enter fractions of a second: 
+45
+
+your time is
+0:0:45
+
+Task complete... Program terminating
+bash-3.2$ ./lab3
+Welcome to my Application!
+--------------------------
+
+enter elapsed time in whole seconds: 
+6543
+
+your time is
+109:0:3
+
+Task complete... Program terminating
+bash-3.2$ ./lab3
+Welcome to my Application!
+--------------------------
+
+enter elapsed time in whole seconds: 
+6543
+109
+your time is
+109:0:3
+
+Task complete... Program terminating
+bash-3.2$ ./lab3
+Welcome to my Application!
+--------------------------
+
+enter elapsed time in whole seconds: 
+6543
+1
+your time is
+1:49:3
+
+Task complete... Program terminating
+bash-3.2$ ./lab3
+Welcome to my Application!
+--------------------------
+
+enter elapsed time in whole seconds: 
+61
+0
+your time is
+0:1:1
+
+Task complete... Program terminating
+bash-3.2$ ./lab3
+Welcome to my Application!
+--------------------------
+
+enter elapsed time in whole seconds: 
+3661
+1
+your time is
+1:1:1
+
+Task complete... Program terminating
+bash-3.2$ ./lab3
+Welcome to my Application!
+--------------------------
+
+enter elapsed time in whole seconds: 
+9451
+
+your time is
+2:37:31
+
+Task complete... Program terminating
+bash-3.2$ ./lab3
+Welcome to my Application!
+--------------------------
+
+enter elapsed time in whole seconds: 
+9451
+2251
+your time is
+2:37:31
+
+Task complete... Program terminating
+bash-3.2$ ./lab3 
+Welcome to my Application!
+--------------------------
+
+enter elapsed time in whole seconds: 
+9630
+
+your time is
+2:40:30
+
+Task complete... Program terminating
+bash-3.2$ ./lab3
+Welcome to my Application!
+--------------------------
+
+enter elapsed time in whole seconds: 
+9630
+
+your time is
+2:145800:30
+
+Task complete... Program terminating
+bash-3.2$ ./lab3
+Welcome to my Application!
+--------------------------
+
+enter elapsed time in whole seconds: 
+9630
+24304030
+your time is
+2:40:30
+
+Task complete... Program terminating
+bash-3.2$ ./lab3
+Welcome to my Application!
+--------------------------
+
+enter elapsed time in whole seconds: 
+9630
+2430
+40
+30
+
+your time is
+2:40:30
+
+Task complete... Program terminating
+bash-3.2$ ./lab3
+Welcome to my Application!
+--------------------------
+
+enter elapsed time in whole seconds: 
+200096213
+
+your time is
+55582:16:53
+
+Task complete... Program terminating
+bash-3.2$ ./lab3
+Welcome to my Application!
+--------------------------
+
+enter elapsed time in whole seconds: 
+9630
+
+your time is
+2:40:30
+
+Task complete... Program terminating
+bash-3.2$ 
