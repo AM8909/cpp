7a89cdb70b1c82d602433f16f9e852d40ec7de17
John Toniolo <john_toniolo9@hotmail.com>
2016-09-29 17:13:01 -0400

    updated template
    added some header files and fixed getchar() for template.cpp

--- template.cpp
+++ template.cpp
@@ -1,6 +1,5 @@
 //==========================================================
 //
-//  Title:    
 //  Course:     CSC 1101
 //  Lab Number: 
 //  Author: 
@@ -8,16 +7,17 @@
 //  Description:
 // 
 // ==========================================================
-//#define NDEBUG       //Disables assert() so program can be used normally
-//#include <cassert>   //Allows assert() function
+//#define NDEBUG      //disables "assert();" so the program can be used after debugging normally
+//#include <cassert>  //allows use of assert()
 //#include <conio.h>   //For function getch() *not needed for linux based compilations
-//#include <cstdlib>  // For several general-purpose functions
+//#include<cstdlib>  // For several general-purpose functions
 //#include <fstream>  // For file handling
 //#include <iomanip>  // For formatted output
 #include <iostream>  // For cin, cout, and system
-//#include <stdio.h>   // Allows use of getchar()
+#include <stdio.h>   // Allows use of getchar()
 //#include <unistd.h> //for some unix tools including sleep
 //#include <string>  // For string data type
+//#include <cmath>   //for abs, pow, and other complex math operations
 
 using namespace std;  // So "std::cout" may be abbreviated to "cout", for example.
 
@@ -29,9 +29,10 @@ int main()
   cout << "--------------------------" << endl << endl;
 
   // Show application close
-  cout << "type any key to exit" << endl;
-  getchar();//holds application window open untill user hits enter
-  return 0;
+  cout << "type any key to exit..." << endl;
+  cin.ignore(10000, '\n'); //clears the input stream buffer for getchar()
+  getchar(); //holds application window open untill user hits enter
+  return 0; 
              
 }
 
