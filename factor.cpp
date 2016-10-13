8ea3652bf27bc042a2a8f90e28263f6edf08c369
John Toniolo <john_toniolo9@hotmail.com>
2016-10-13 16:52:59 -0400

    factor cleaned up

--- factor.cpp
+++ factor.cpp
@@ -8,10 +8,8 @@
 // ==========================================================
 #include <iostream>  // For cin, cout, and system
 #include <stdio.h>   // Allows use of getchar()
-#include <cmath>     //for math functions
-#include <iomanip>   //for formatting output in a neeter format
-
-using namespace std;  // So "std::cout" may be abbreviated to "cout", for example.
+#include <cmath>     // For fmod() or % on doubles
+using namespace std; 
 
 int main()
 {
@@ -19,62 +17,56 @@ int main()
   double
     userIn,       //user supplied value to be tested for primacy
     t,            //will store an odd numbered divisor
-    y,            //will store an even numbered divisor
-    userInModEv,  //will store quotit of 'userIn / 2'
-    n;            //will be used as a flag
-  
-  
+    n;            //will be used as a flag 
+    
   // Show application header
   cout << "-------factor finder-------" << endl
        << "enter a positive integer" << endl
        << "enter 0 to exit" << endl << endl;
   
   //keep prompting user untill 0 is entered
-  while(userIn != 0){   
-  //be sure to reset variables at beginning of while loop
-  //or the loop will just hang after first run
-  n = 0;
-  t = 1.0;
-  y = 0.0;
+  while(userIn != 0){ 
+  
+  //reset variables at beginning of while loop
+  //or the loop will hang after first run
+  t = 1;
+  n = 1;
   cin >> userIn;
-  userInModEv = fmod(userIn, 2);
+  
   //check for 1-7
   if (userIn == 1 ||
       userIn == 2 ||
       userIn == 3 ||
       userIn == 5 ||
       userIn == 7){
-    cout << "prime" << endl;           
+    cout << "prime" << endl;
+    //n = 0 so it cannot be flagged as prime twice
+    n = 0;
   }
   
-  //test for even, exclude 0
-  else if (userInModEv == 0 &&
-	   userIn != 0){         
-    while (y < userIn){
-      y = y + 2;
-      if (fmod(userIn, y) == 0){
-	cout << "factor " << y << endl;
-      }
-    }
-  }
-   
-  //test for odd, exclude 0
-  else if (userInModEv != 0){         
+  else{
     while (t < userIn){
-      t = t + 2;    
-      //divide by 3, 5, 7 ect...
-      if (fmod(userIn, t) == 0){      
-	cout << "factor " << t << endl;
-	n = 1;
+      t = t + 1;
+      //check each intiger up to userIn
+      if (fmod(userIn, t) == 0 && t < userIn){      
+	cout << "factor " << "\t" 
+	     << "\t" << t << endl;
+	//n = 0 so cannot be flagged as prime
+	n = 0;
       }
-      //if you run out of factors to test
+      //if it runs out of factors to test
       //and userIn doesn't factor to an integer
-      else if (t >= userIn && n != 1){
+      else if (t >= userIn && n != 0){
 	cout << "prime" << endl;
       }
-    }//end of odd number while
-  }//end of odd number iff
-  }//end of user prompt while
+     } //end of while
+    }//end of else
+   }//end of user prompt while
+  
+  //closing promt when 0 is entered
+  cout << "application terminating..." << endl;
   return 0;
-}
+}//end of main
+
+
 
